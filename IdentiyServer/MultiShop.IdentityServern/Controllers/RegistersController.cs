a7248af915c88d644ac5fb0abca6c1284b901923
Abdullah KOCDEMİR <kcdmirapo96@gmail.com>
2025-07-20 00:08:10 +0300

    Jwt Ve Identityserver Eklendi

---
+++ IdentiyServer/MultiShop.IdentityServern/Controllers/RegistersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServern.Dtos;
+using MultiShop.IdentityServern.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser(UserRegisterDtos userRegisterDtos)
+        {
+            var value = new ApplicationUser()
+            {
+                UserName = userRegisterDtos.Username,
+                Name = userRegisterDtos.Name,
+                SurName = userRegisterDtos.Surname,
+                Email = userRegisterDtos.Email
+            };
+            var result = await _userManager.CreateAsync(value, userRegisterDtos.Password);
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı Başarıyla Eklendi");
+            }
+            else
+            {
+                return Ok("Bir Hata Oluştu Tekrar Deneyiniz");
+            }
+        }
+    }
+}
