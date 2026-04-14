6de4c52f7daec49cb65df037896493161058b4e5
Abdullah Kocdemir <abdullahkocdemir@example.com>
2026-04-15 00:40:52 +0300

    IdentityServer Eklendi

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDTO userRegisterDTO)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = userRegisterDTO.Username,
+                Email = userRegisterDTO.Email,
+                Name = userRegisterDTO.Name,
+                Surname = userRegisterDTO.Surname
+            };
+            var result = await _userManager.CreateAsync(user, userRegisterDTO.Password);
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı Başarılı Bir Şekilde Kaydedildi.");
+            }
+            return Ok("Kullanıcı Kaydı Başarısız Oldu.");
+        }
+    }
+
+}
